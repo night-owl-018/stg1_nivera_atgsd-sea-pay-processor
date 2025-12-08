63091c410df4ccbfbb1e0886aa086a22c7b92aaa
nightowl <34909304+night-owl-018@users.noreply.github.com>
2025-12-07 18:31:43 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder="web/frontend")
+    app = Flask(
+        __name__,
+        template_folder="web/frontend",
+        static_folder="web/frontend"
+    )
 
     from .routes import bp
     app.register_blueprint(bp)
